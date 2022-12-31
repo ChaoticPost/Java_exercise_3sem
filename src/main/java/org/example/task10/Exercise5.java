172b1500a2e9eeb01f299d715c27a397fbaa8af2
DariaChugunova <dariachugunova2003@gmail.com>
2022-12-31 16:58:47 +0300

    Commit

---
+++ src/main/java/org/example/task10/Exercise5.java
@@ -0,0 +1,26 @@
+package org.example.task10;
+
+import java.util.Scanner;
+
+/**
+ * Сумма цифр числа
+ * Дано натуральное число N. Вычислите сумму его цифр.
+ * 46
+ * При решении этой задачи нельзя использовать строки, списки, массивы (ну
+ * и циклы, разумеется).
+ */
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sumOfDigits(sc.nextInt()));
+    }
+
+    public static int sumOfDigits(int x) {
+        if (x > 9) {
+            return x % 10 + sumOfDigits(x / 10);
+        } else {
+            return x;
+        }
+    }
+}
