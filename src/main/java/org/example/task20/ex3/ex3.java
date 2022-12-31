f14402595a6165276201c48fac4f346fa2886f3f
DariaChugunova <dariachugunova2003@gmail.com>
2022-12-31 18:37:03 +0300

    Commit

---
+++ src/main/java/org/example/task20/ex3/ex3.java
@@ -0,0 +1,17 @@
+package org.example.task20.ex3;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class ex3 {
+    public static void main(String[] args) {
+        File dir = new File("D:\\ideaJAVA"); //path указывает на директорию
+        File[] arrFiles = dir.listFiles();
+        List<File> lst = Arrays.asList(arrFiles);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(lst.get(i));
+        }
+    }
+}
