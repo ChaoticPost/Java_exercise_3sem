2574a3cb005a859de9edf9f016c9c33223e6eb64
DariaChugunova <dariachugunova2003@gmail.com>
2022-12-31 19:32:14 +0300

    Commit

---
+++ src/main/java/org/example/task26/ArrayQueue.java
@@ -0,0 +1,38 @@
+package org.example.task26;
+
+import java.util.ArrayList;
+
+public class ArrayQueue<E> extends AbstractQueue<E> {
+    protected ArrayList<E> data;
+
+    public ArrayQueue() {
+        this.data = new ArrayList<>();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return data.add(e);
+    }
+
+    @Override
+    public E poll() {
+        if (data.isEmpty()) {
+            return null;
+        }
+
+        E e = data.get(0);
+        data.remove(0);
+        return e;
+    }
+
+    @Override
+    public E peek() {
+        return data.isEmpty() ? null : data.get(0);
+    }
+}
+
---
+++ src/main/java/org/example/task26/LinkedQueue.java
@@ -0,0 +1,38 @@
+package org.example.task26;
+
+import java.util.LinkedList;
+
+public class LinkedQueue<E> extends AbstractQueue<E> {
+    protected LinkedList<E> data;
+
+    public LinkedQueue() {
+        this.data = new LinkedList<>();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return data.add(e);
+    }
+
+    @Override
+    public E poll() {
+        if (data.isEmpty()) {
+            return null;
+        }
+
+        E e = data.getFirst();
+        data.removeFirst();
+        return e;
+    }
+
+    @Override
+    public E peek() {
+        return data.isEmpty() ? null : data.get(0);
+    }
+}
+
