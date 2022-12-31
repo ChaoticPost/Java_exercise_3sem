492d8cf2bdb74e35affc562fee2b2f354b95e888
DariaChugunova <dariachugunova2003@gmail.com>
2022-12-31 15:29:19 +0300

    Commit

---
+++ src/main/java/org/example/task8/ex1/Animation.java
@@ -0,0 +1,25 @@
+package org.example.task8.ex1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Animation extends JFrame {
+
+    public Animation() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setTitle("Fire animation");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(180, 200));
+        getRootPane().setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        add(new FireSprite());
+        pack();
+    }
+
+    public static void main(String[] args) {
+        new Animation().setVisible(true);
+    }
+}
+
